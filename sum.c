9adab40139d927587dcd9b6c12e7cca5b06ae51e
201710100761 <60249698+201710100761@users.noreply.github.com>
2020-01-24 10:53:55 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int i,j,n,prod,sum;
+printf("Enter the number\n");
+scanf("%d",&n);
+sum=0;
+for(i=1;i<=n;i++)
+{
+	 prod=1;
+	 for(j=1;j<=i;j++)
+	{
+	prod = prod*i;
+	}
+	sum=sum+prod;
+}
+
+ printf("Sum=%d\n",sum);
+}
